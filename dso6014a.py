040bdf31d4c1981308ea746cf90c99359ad74d12
EllieCinderelly <EllieRoth2757@outlook.com>
2025-01-19 21:18:34 -0600

    Files for SSHing into RPi, sending SPi, and controlling the Agilent DSO014A

---
+++ dso6014a.py
@@ -0,0 +1,48 @@
+import pyvisa as visa
+import sys
+import spidev
+
+# Intended use for this file is SSH control to RPi SPi outputs and controlling the Agilent DSO6014A 
+# windows python ssh -> rpi spidev and pyvisa-py
+# 
+# windows IVI drivers for DSO6014A seem to be very deprecated. Pyvisa-py on rpi4 is proving best here
+
+
+ID = 'USB0::2391::5924::MY45007512::0::INSTR'
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 9600
+spi.mode = 0
+
+def measure(ID,words):
+
+    rm = visa.ResourceManager('@py')
+    oscope = rm.open_resource(ID)
+    oscope.timeout = None
+    instrument_ID = oscope.query('*IDN?')
+    print('Instrument Info:\n%s' % (instrument_ID))
+    spi_send(words)
+    oscope.close()
+    rm.close()
+
+def spi_send(spi_arr):
+    to_send = []*len(spi_arr)
+    for i in range(len(spi_arr)):
+            print(spi_arr)
+            print(spi_arr[i])
+            print(int(spi_arr[i],16))
+            to_send.append(int(spi_arr[i],16))
+    
+    print(to_send)    
+    spi.xfer(to_send)
+    print('Sent %s' % (spi_arr))
+
+
+if __name__ == '__main__':
+
+    n = []
+    for i in range(1, len(sys.argv), 1):
+        n.append(sys.argv[i])
+
+    measure(ID,n)
---
+++ main.py
@@ -0,0 +1,40 @@
+import paramiko
+
+host = ""
+username = ""
+password = ""
+
+def run_main():
+    return input('Enter something to send:')
+
+def send(arg):
+    print(host,username)
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password)
+    _stdin, _stdout,_stderr = client.exec_command("sudo python /home/ellie/code/dso6014a/dso6014a.py %s" % (arg))
+    print(_stdout.read().decode())
+    client.close()
+
+def init_ssh():
+    global host
+    global username
+    file = open('ssh-info.txt','r')
+    info = file.readlines()
+    ip = str(info[0])
+    name = str(info[1])
+    host = ip.strip()
+    username = name.strip()
+
+if __name__ == '__main__':
+    try:
+        init_ssh()
+    except:
+        print('Please create ssh-info.txt in main.py directory and add SSH host IP on the first line\n and the ssh username on the second line.\n')
+    else:
+        while(1):
+            to_send = run_main()
+            if(to_send == 'exit'): 
+                break
+            else:
+                send(to_send)
